2df2ed2e4030e812a5ba6d5ea70453e20762a9fb
Douglas Blackwood <douglas.blackwood@protonmail.com>
2020-08-15 14:11:58 +0200

    Black

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -4,9 +4,9 @@ from devnews.interfaces import NewsRepository
 
 
 def test_abstract():
-    with pytest.raises(
-        TypeError,
-        match="Can't instantiate abstract class NewsRepository with abstract methods get_all",
-    ):
+    error_string = (
+        "Can't instantiate abstract class NewsRepository with abstract methods get_all"
+    )
+    with pytest.raises(TypeError, match=error_string):
         repo = NewsRepository()
         repo.get_all()

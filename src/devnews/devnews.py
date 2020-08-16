8512fe7b6a6bf62f8a4ca8bd02e8bbff9f0cca9c
Douglas Blackwood <douglas.blackwood@protonmail.com>
2020-08-16 15:17:33 +0200

    Suppression code mort

--- src/devnews/devnews.py
+++
@@ -1,8 +0,0 @@
-from terminaltables import SingleTable
-
-
-def print_table(news):
-    data = [(entry.source_name, entry.title) for entry in news[:10]]
-    data.insert(0, ("Feed", "Title"))
-    table = SingleTable(data)
-    print(table.table)
